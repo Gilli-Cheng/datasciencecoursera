971bf29f036e0b8189290cc28dcf3a3d8fb33921
Gilli-Cheng <gillibeanbean00@gmail.com>
2021-07-05 16:31:30 +0800

    Initial commit

---
+++ dumptry.R
@@ -0,0 +1,5 @@
+x <-
+"w"
+y <-
+structure(list(a = 1, b = "a"), class = "data.frame", row.names = c(NA, 
+-1L))
---
+++ pollutantmean.R
@@ -0,0 +1,8 @@
+pollutantmean <- mean(directory, pollutant, id = 1:332){
+        fulldir <- paste(getwd(),"/",directory, sep = "")
+                file
+}
+read.csv("200.csv")
+sulfate <- file[ ,2]
+nitrate <- file[ ,3]
+mean(nitrate, id)
