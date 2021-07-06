3344478363cd2f3e29b28a1e827f23721e3f6eca
Gilli-Cheng <gillibeanbean00@gmail.com>
2021-07-06 08:51:19 +0800

    my first complete function

--- r-programming/pollutantmean.R
+++ r-programming/pollutantmean.R
@@ -1,8 +1,21 @@
-pollutantmean <- mean(directory, pollutant, id = 1:332){
-        fulldir <- paste(getwd(),"/",directory, sep = "")
-                file
+pollutantmean <- function(directory, pollutant, id){
+        # the path , check 
+        directory <- paste(getwd(),"/",directory, "/",sep = "")
+        file_list <- list.files(directory)
+        blank <- data.frame()
+        
+        for(i in id){
+                files <- read.csv(paste(directory,file_list[i],sep = ""))
+                blank <- rbind(blank, files)
+        }
+        mean(blank[[pollutant]],na.rm = TRUE)
 }
-read.csv("200.csv")
-sulfate <- file[ ,2]
-nitrate <- file[ ,3]
-mean(nitrate, id)
+test <- pollutantmean("specdata","sulfate",id = 1:10)
+test
+# 我需要資料夾路徑
+# 需要把各個資料，依著col和id整併成一個檔案
+# 需要能算平均
+# mean(某個新資料, id)
+# 還要排除掉NA !is.NA() 或是 na.rm = TRUE
+
+
